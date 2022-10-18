37cca6bbec914ea84235cc46267db6e172e52673
su <rakdav@inbox.ru>
2022-10-18 11:03:18 +0200

    Добавьте файлы проекта.

---
+++ ParallelObject/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace ParallelObject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CancellationTokenSource _cancelToken =
+            new CancellationTokenSource();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdProcess_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Factory.StartNew(() => ProcessFiles());
+            this.Title = "Processing Complete";
+        }
+
+        private void cmdCancel_Click(object sender, RoutedEventArgs e)
+        {
+            _cancelToken.Cancel();
+        }
+        private void ProcessFiles()
+        {
+            ParallelOptions parOpts = new ParallelOptions();
+            parOpts.CancellationToken = _cancelToken.Token;
+            parOpts.MaxDegreeOfParallelism = System.Environment.ProcessorCount;
+            var basePath = Directory.GetCurrentDirectory();
+            var pictureDirectory = Path.Combine(basePath, "TestPictures");
+            var outputDirectory = Path.Combine(basePath, "ModifiedPictures");
+            if (Directory.Exists(outputDirectory))
+            {
+                Directory.Delete(outputDirectory, true);
+            }
+            Directory.CreateDirectory(outputDirectory);
+            string[] files = Directory.GetFiles(
+                pictureDirectory, "*.jpg", SearchOption.AllDirectories);
+            try
+            {
+                Parallel.ForEach(files,parOpts,currentFile =>
+                {
+                    parOpts.CancellationToken.ThrowIfCancellationRequested();
+                    string filename = Path.GetFileName(currentFile);
+                    Dispatcher?.Invoke(() =>
+                    {
+                        this.Title = $"Processing {filename} " +
+                        $"on thread {Thread.CurrentThread.ManagedThreadId}";
+                    });
+                    using (Bitmap bitmap = new Bitmap(currentFile))
+                    {
+                        bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                        bitmap.Save(Path.Combine(outputDirectory, filename));
+                    }
+                });
+                Dispatcher?.Invoke(() => this.Title = "Done!");
+            }
+            catch (OperationCanceledException ex)
+            {
+                Dispatcher?.Invoke(() => this.Title = ex.Message);
+            }
+        }
+    }
+}
